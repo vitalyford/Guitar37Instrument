b123893acb477fc6cb0d28b91b70262a0fc5ba7e
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2020-08-18 16:52:10 -0400

    Initial commit

---
+++ Instrument.java
@@ -0,0 +1,34 @@
+public interface Instrument {
+    /**
+     * Initializes the sample frequency buffer with random numbers
+     * in a range of [-0.5; 0.5] for every string.
+     * 
+     * @param key Defines the key pressed by the user that you want to
+     * correspond to a specific string on your "Guitar 37" instrument.
+     * The easiest way to process it is to create an alphabet of 37 keys
+     * and use the corresponding index of the key in the alphabet as a
+     * reference to a specific string on your "Guitar 37" instrument.
+     * However, it is up to you how you want to implement it.
+     */
+    public void pluck(char key);
+
+    /**
+     * Provides a time simulation that advances the simulation one step forward
+     * according to the Karplus-Strong update algorithm that:
+     * 
+     * 1. Takes the average of the <b>first two</b> frequency samples for every string.
+     * 2. Multiplies that average by the DECAY_FACTOR set to, e.g., 0.994.
+     * 3. Adds the result to the end of the buffer.
+     * 4. Removes the sample frequency at the front of the buffer.
+     * 
+     * Note that the DECAY_FACTOR can be changed but be careful with how much
+     * you tweak that number :)
+     */
+    public void tick();
+
+    /**
+     * Returns the sum of the values from the front of the buffer of
+     * every available string.
+     */
+    public double superposition();
+}
---
+++ PlayInstrument.java
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Name: 
+ * 
+ * Description: Plays guitar strings when the user presses 
+ * certain keys on the keyboard. Karplus-Strong algorithm is used to
+ * synthesize the sound of a plucked string.
+ * 
+ * In the constructor of the class implementing Instrument,
+ * you will initialize Concert A frequency as 440. Based on that,
+ * you will create "Guitar 37" strings, every frequency of which 
+ * will be defined as:
+ * 
+ * frequency = CONCERT_A * Math.pow(2.0, (i - 24) / 12.0)
+ * 
+ * where i is within [0, 37) range. For every one of 37 strings,
+ * you will allocate a "buffer" (LinkedList<Double>) the size of which
+ * is defined as:
+ * 
+ * size = 44100 / frequency
+ * 
+ * The constructor will initialize all buffers of all strings with zeros.
+ *
+ ******************************************************************************/
+public class PlayInstrument {
+    public static void main(String[] args) {
+
+        Instrument guitar = new AsianMalaysian();
+
+        while (true) {
+
+            // check if the user has pressed a key, and, if so, process it
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+
+                // pluck the corresponding string
+                guitar.pluck(key);
+            }
+
+            // compute the superposition of the samples
+            double sample = guitar.superposition();
+
+            // send the result to standard audio
+            StdAudio.play(sample);
+
+            // advance the simulation of each guitar string by one step
+            guitar.tick();
+        }
+    }
+}
