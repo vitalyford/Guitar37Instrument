c701f0d0b68782c6db8f5287406c06d30f7d26a0
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2020-08-18 21:12:25 -0400

    Update formatting

--- Instrument.java
+++ Instrument.java
@@ -1,14 +1,14 @@
 public interface Instrument {
     /**
-     * Initializes the sample frequency buffer with random numbers
-     * in a range of [-0.5; 0.5] for every string.
+     * Initializes the sample frequency buffer with random numbers in a range of
+     * [-0.5; 0.5] for every string.
      * 
-     * @param key Defines the key pressed by the user that you want to
-     * correspond to a specific string on your "Guitar 37" instrument.
-     * The easiest way to process it is to create an alphabet of 37 keys
-     * and use the corresponding index of the key in the alphabet as a
-     * reference to a specific string on your "Guitar 37" instrument.
-     * However, it is up to you how you want to implement it.
+     * @param key Defines the key pressed by the user that you want to correspond to
+     *            a specific string on your "Guitar 37" instrument. The easiest way
+     *            to process it is to create an alphabet of 37 keys and use the
+     *            corresponding index of the key in the alphabet as a reference to a
+     *            specific string on your "Guitar 37" instrument. However, it is up
+     *            to you how you want to implement it.
      */
     public void pluck(char key);
 
@@ -16,19 +16,19 @@ public interface Instrument {
      * Provides a time simulation that advances the simulation one step forward
      * according to the Karplus-Strong update algorithm that:
      * 
-     * 1. Takes the average of the <b>first two</b> frequency samples for every string.
-     * 2. Multiplies that average by the DECAY_FACTOR set to, e.g., 0.994.
-     * 3. Adds the result to the end of the buffer.
-     * 4. Removes the sample frequency at the front of the buffer.
+     * 1. Takes the average of the <b>first two</b> frequency samples for every
+     * string. 2. Multiplies that average by the DECAY_FACTOR set to, e.g., 0.994.
+     * 3. Adds the result to the end of the buffer. 4. Removes the sample frequency
+     * at the front of the buffer.
      * 
-     * Note that the DECAY_FACTOR can be changed but be careful with how much
-     * you tweak that number :)
+     * Note that the DECAY_FACTOR can be changed but be careful with how much you
+     * tweak that number :)
      */
     public void tick();
 
     /**
-     * Returns the sum of the values from the front of the buffer of
-     * every available string.
+     * Returns the sum of the values from the front of the buffer of every available
+     * string.
      */
     public double superposition();
 }
--- PlayInstrument.java
+++ PlayInstrument.java
@@ -1,20 +1,18 @@
 /******************************************************************************
- * Name: 
+ * Name:
  * 
- * Description: Plays guitar strings when the user presses 
- * certain keys on the keyboard. Karplus-Strong algorithm is used to
- * synthesize the sound of a plucked string.
+ * Description: Plays guitar strings when the user presses certain keys on the
+ * keyboard. Karplus-Strong algorithm is used to synthesize the sound of a
+ * plucked string.
  * 
- * In the constructor of the class implementing Instrument,
- * you will initialize Concert A frequency as 440. Based on that,
- * you will create "Guitar 37" strings, every frequency of which 
- * will be defined as:
+ * In the constructor of the class implementing Instrument, you will initialize
+ * Concert A frequency as 440. Based on that, you will create "Guitar 37"
+ * strings, every frequency of which will be defined as:
  * 
  * frequency = CONCERT_A * Math.pow(2.0, (i - 24) / 12.0)
  * 
- * where i is within [0, 37) range. For every one of 37 strings,
- * you will allocate a "buffer" (LinkedList<Double>) the size of which
- * is defined as:
+ * where i is within [0, 37) range. For every one of 37 strings, you will
+ * allocate a "buffer" (LinkedList<Double>) the size of which is defined as:
  * 
  * size = 44100 / frequency
  * 
